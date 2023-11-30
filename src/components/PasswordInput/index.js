0946ab6d9a7b6a2f9784f3482667266c58150e97
sulnoorman <noormansulthan5@gmail.com>
2023-11-30 12:09:42 +0700

    add page login

--- src/components/PasswordInput/index.js
+++ src/components/PasswordInput/index.js
@@ -1,27 +1,28 @@
 import React from "react";
+import { FaRegEye } from "react-icons/fa6";
 
-const InlineIconInput = ({ value = '', type = 'text', onchange = () => { }, iconPrefix = '', iconSuffix = '', placeholder = 'Ketikkan Sesuatu' }) => {
+const PasswordInput = ({ value = '', onchange = () => { }, iconPrefix = '', showHideButton = true, placeholder = 'Ketikkan Sesuatu' }) => {
     return (
         <div class="relative flex items-center w-full">
             <input
-                type={type}
+                type="password"
                 value={value}
                 onChange={onchange}
                 placeholder={placeholder}
-                className={`peer relative h-10 w-full rounded-md bg-gray-50 ${iconPrefix !== '' ? 'pl-12' : 'pl-4'} ${iconSuffix !== '' ? 'pr-10' : 'pr-4'} outline-none drop-shadow-sm transition-all duration-200 ease-in-out focus:bg-white focus:ring-2 focus:ring-blue-400 focus:drop-shadow-lg`}
+                className={`peer relative h-10 w-full rounded-md bg-gray-50 ${iconPrefix !== '' ? 'pl-12' : 'pl-4'} ${showHideButton ? 'pr-10' : 'pr-4'} outline-none drop-shadow-sm transition-all duration-200 ease-in-out focus:bg-white focus:ring-2 focus:ring-blue-400 focus:drop-shadow-lg`}
             />
-            {iconPrefix !== '' &&
+            {iconPrefix &&
                 <span className="material-symbols-outlined absolute left-2 transition-all duration-200 ease-in-out group-focus-within:text-blue-400">
                     {iconPrefix}
                 </span>
             }
-            {iconSuffix !== '' &&
+            {showHideButton &&
                 <span className="material-symbols-outlined absolute right-2 transition-all duration-200 ease-in-out group-focus-within:text-blue-400">
-                    {iconSuffix}
+                    <FaRegEye size={18} style={{ cursor: 'pointer' }} />
                 </span>
             }
         </div>
     )
 }
 
-export default InlineIconInput;
+export default PasswordInput;
--- src/pages/Auth/Login/index.js
+++ src/pages/Auth/Login/index.js
@@ -3,6 +3,8 @@ import { Button } from "../../../components/Button";
 import { Navbar } from "../../../components/Navbar";
 import { Footer } from "../../../components/Footer";
 import InlineIconInput from "../../../components/InlineIconInput";
+import { FaRegEnvelope, FaUnlockKeyhole } from "react-icons/fa6";
+import PasswordInput from "../../../components/PasswordInput";
 // import masukPng from './assets/img/masuk.png';
 
 const LoginPage = () => {
@@ -23,13 +25,24 @@ const LoginPage = () => {
                         <img alt="" src={'/assets/img/masuk.png'} />
                     </div>
                     <div className="p-2 flex item-center justify-center">
-                        <div style={{ backgroundColor: '#FFB84C' }} className="px-4 py-2 w-[470px] rounded-full flex flex-col justify-center gap-10">
+                        <div style={{ backgroundColor: '#FFB84C' }} className="px-10 py-2 w-[568px] rounded-full flex flex-col justify-center gap-10">
                             <div className="text-center">
                                 <h1 className="text-[35px] text-blue-900 font-bold">Masuk</h1>
                             </div>
                             <div className="flex flex-col gap-5">
-                                <InlineIconInput value={uname} icon="mail" onchange={(e) => setUname(e.target.value)} />
-                                <InlineIconInput value={password} icon="password" onchange={(e) => setPassword(e.target.value)} />
+                                <InlineIconInput
+                                    value={uname}
+                                    placeholder="Email/Username"
+                                    iconPrefix={<FaRegEnvelope size={25} />}
+                                    onchange={(e) => setUname(e.target.value)}
+                                />
+                                <PasswordInput
+                                    showHideButton
+                                    value={password}
+                                    placeholder="Password"
+                                    iconPrefix={<FaUnlockKeyhole size={25} />}
+                                    onchange={(e) => setPassword(e.target.value)}
+                                />
                                 <Button title={'Masuk'} bgColor={'bg-blue-900'} onClick={handleLogin} />
                             </div>
                             <div className="flex flex-col justify-center">
@@ -37,13 +50,13 @@ const LoginPage = () => {
                                     <div className="flex flex-1 justify-end">
                                         <h6 className="font-bold">Belum Punya Akun?</h6>
                                     </div>
-                                    <h6 className="font-bold text-blue-900 flex-1">Register</h6>
+                                    <h6 style={{ cursor: 'pointer' }} className="font-bold text-blue-900 flex-1 hover:text-blue-700 hover:underline">Register</h6>
                                 </div>
                                 <div className="flex flex-1 flex-row gap-4">
                                     <div className="flex flex-1 justify-end">
                                         <h6 className="font-bold">Lupa Password?</h6>
                                     </div>
-                                    <h6 className="font-bold text-blue-900 flex-1">Reset Password</h6>
+                                    <h6 style={{ cursor: 'pointer' }} className="font-bold text-blue-900 flex-1 hover:text-blue-700 hover:underline">Reset Password</h6>
                                 </div>
                             </div>
                         </div>
