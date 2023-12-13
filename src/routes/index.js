b8e0dacd84d2312d04e0f128f6358d4bbfd20e9f
deffinad <deffinjr890@gmail.com>
2023-12-13 22:28:22 +0700

    change cors

--- src/routes/index.js
+++ src/routes/index.js
@@ -12,7 +12,7 @@ var router = express.Router();
 router.use(bodyParser.json());
 
 const corsOptions = {
-    origin: "http://localhost:3000",
+    origin: "http://localhost:3001",
     credentials: true, //access-control-allow-credentials:true
     optionSuccessStatus: 200,
 };
