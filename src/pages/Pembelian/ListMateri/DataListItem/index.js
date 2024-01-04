e4946b8e41b064fd8154f291007bdd1b9d53fe17
deffinad <deffinjr890@gmail.com>
2024-01-04 08:54:06 +0700

    change table list materi

--- src/pages/Pembelian/ListMateri/DataListItem/index.js
+++ src/pages/Pembelian/ListMateri/DataListItem/index.js
@@ -24,7 +24,7 @@ const DataListItem = ({ data, index, setRefresh }) => {
         <Fragment>
             <tr className='bg-white hover:bg-gray-100 cursor-pointer'>
                 <td className="w-[5%] px-3 py-3 border-b border-gray-200 text-sm">
-                    <p className="text-gray-900 text-lg whitespace-no-wrap text-center">{index + 1}.</p>
+                    <p className="text-gray-900 text-lg whitespace-no-wrap">{index + 1}.</p>
                 </td>
                 <td className="w-[25%] px-3 py-3 border-b border-gray-200 text-sm">
                     <p className="text-gray-900 text-lg whitespace-no-wrap">{data.user.nama}</p>
@@ -32,11 +32,15 @@ const DataListItem = ({ data, index, setRefresh }) => {
                 <td className="w-[20%] px-3 py-3 border-b border-gray-200 text-sm">
                     <p className="text-gray-900 text-lg whitespace-no-wrap">{data.produk.nama}</p>
                 </td>
-                <td className="w-[20%] px-3 py-3 border-b border-gray-200 text-sm text-center">
+                <td className="w-[20%] px-3 py-3 border-b border-gray-200 text-sm">
                     <p className="text-gray-900 text-lg whitespace-no-wrap">{data.tanggal}</p>
                 </td>
                 <td className="w-[20%] px-3 py-3 border-b border-gray-200 text-sm">
-                    <p className="text-gray-900 text-lg whitespace-no-wrap">{data.status}</p>
+                    <div className='flex flex-row justify-start items-center'>
+                        <div className={`rounded ${data.status === 'menunggu pembayaran' ? 'bg-yellow-400' : data.status === 'berhasil' ? 'bg-blue-400' : 'bg-red-400'} text-white px-2 flex flex-row items-center`}>
+                            <p className="text-lg whitespace-no-wrap">{data.status}</p>
+                        </div>
+                    </div>
                 </td>
                 <td className="w-[10%] px-3 py-3 border-b border-gray-200 text-sm">
                     <div className="flex flex-row gap-x-3 gap-y-0">
