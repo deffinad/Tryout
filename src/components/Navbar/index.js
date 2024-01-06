44b2b169134778446556cd2db9a56fd47f4abb92
deffinad <deffinjr890@gmail.com>
2024-01-06 19:00:16 +0700

    handle avatar navbar and change title

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import { FaBars, FaXmark } from 'react-icons/fa6'
 import useAuth from "../../shared/hooks";
 
 export const Navbar = ({ toggle = false, setToggle }) => {
     const { user } = useAuth()
-    
+    const currentUserData = JSON.parse(localStorage.getItem('user'));
+
+    const renderAvatar = useCallback(() => {
+        if (currentUserData) {
+            if (currentUserData.avatar !== '') return currentUserData?.avatar;
+            return '/assets/img/avatar.png'
+        }
+
+    }, [currentUserData])
+
     return (
         <div className='w-full bg-primary h-[90px] flex flex-row items-center justify-between text-textColor px-16 fixed z-10'>
             <div className='flex gap-6 items-center'>
@@ -21,7 +30,7 @@ export const Navbar = ({ toggle = false, setToggle }) => {
             </div>
 
             <div>
-                <img className="w-10 h-10 rounded-full" src={user?.avatar} alt="Rounded avatar" />
+                <img className="w-10 h-10 rounded-full" src={renderAvatar()} alt="Rounded avatar" />
             </div>
         </div>
     )
